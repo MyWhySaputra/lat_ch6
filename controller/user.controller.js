71de1c90cbd78c9704ebfb1195326bfdc0108ed8
MyWahyuSap <wahyusaputra222000@gmail.com>
2023-11-14 19:58:44 +0700

    14/11/2023

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -64,8 +64,6 @@ async function Upload(req, res) {
     
     const imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
 
-
-
     return res.status(200).json({
         status: true,
         message: 'success',
